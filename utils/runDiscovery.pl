efad783ccdcd7c980c80d53d5632a16eb5bc307d
henryst57 <henrst@vcu.edu>
2017-11-28 14:45:53 -0500

    changed LiteratureBasedDiscovery.pm to ALBD for CPAN reasons

--- utils/runDiscovery.pl
+++ utils/runDiscovery.pl
@@ -113,7 +113,7 @@ use strict;
 use warnings;
 
 use Getopt::Long;
-use LiteratureBasedDiscovery;
+use ALBD;
 
 ###############################################################################
 # CONSTANT STRINGS
@@ -154,7 +154,7 @@ GetOptions( 'debug'             => \$DEBUG,
  
 #Check for version or help
 if ($VERSION) {
-    print "current version is ".(LiteratureBasedDiscovery->version())."\n";
+    print "current version is ".(ALBD->version())."\n";
     exit;
 }     
 if ($HELP) {
@@ -170,7 +170,7 @@ if ($HELP) {
 $options{'lbdConfig'} = shift;
 defined $options{'lbdConfig'} or die ($usage);
 
-my $lbd = LiteratureBasedDiscovery->new(\%options);
+my $lbd = ALBD->new(\%options);
 $lbd->performLBD();
 
 ############################################################################
